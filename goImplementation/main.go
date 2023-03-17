0788345f2aa7c88db8ff29dea9a52bac1ccb6711
Fb185 <fredbobonejr@gmail.com>
2023-03-17 03:16:58 +0000

    javascript implementation logic complete

---
+++ goImplementation/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+
+func main(){
+    chain := InitBlockChain()
+    chain.AddBlock("First Block")
+    chain.AddBlock("Second Block")
+    chain.AddBlock("Third Block")
+
+    for _, block := range chain.blocks{
+        fmt.Printf("Previous Hash: %x\n", block.PrevHash)
+        fmt.Printf("Data in Block: %s\n", block.Data)
+        fmt.Printf("Hash: %x\n", block.Hash)
+    }
+}
--- javascriptImplementation/src/main.js
+++ javascriptImplementation/src/main.js
@@ -1,82 +1,29 @@
-const SHA256 = require('crypto-js/sha256')
+const {BlockChain, Transaction} = require('./blockchain');
+const EC = require('elliptic').ec
+const ec = new EC('secp256k1')
 
-class Block {
-    constructor(timestamp, medicalRecord, previousHash = '') {
-        this.timestamp = timestamp;
-        this.medicalRecord = medicalRecord;
-        this.previousHash = previousHash;
-        this.hash = this.calculateHash()
-        this.nonce = 0;
+const myKey = ec.keyFromPrivate('719f65a4552ba29e1a253047fee1bee9982856fc27caf6c4d760857ed2746c10')
+const myWalletAddress = myKey.getPublic('hex')
 
-    }
-    calculateHash() {
-        return SHA256(this.previousHash + this.timestamp + this.medicalRecord + this.nonce).toString()
-        // return SHA256(this.previousHash + this.timestamp + JSON.stringify(this.medicalRecord) + this.nonce).toString()
-    }
-
-    mineBlock(difficulty) {
-        while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
-            this.nonce++;
-            this.hash = this.calculateHash();
-        }
-        // console.log("Block mined: " + this.hash)
-    }
-}
-
-class Transaction {
-    constructor(fromAddress, toAddress, data) {
-        this.fromAddress = fromAddress
-        this.toAddress = toAddress
-        this.data = data;
-    }
-
-}
-class BlockChain {
-    constructor() {
-        this.chain = [this.createGenesisBlock()];
-        this.difficulty = 0;
-        this.pendingTransaction = [];
-    }
-
-    createGenesisBlock() {
-        return new Block("01/07/2023", "Genesis Block", "0")
-    }
-
-    getLatestBlock() {
-        return this.chain[this.chain.length - 1];
-    }
+let coin = new BlockChain();
 
 
-    minePendingTransactions(fromAddress, toAddress, data) {
-        let block = new Block(Date.now(), this.pendingTransaction)
-        block.previousHash = this.getLatestBlock().hash
-        block.mineBlock(this.difficulty)
+const tx1 = new Transaction(myWalletAddress, 'publickey goes here', 10)
+tx1.signTransaction(myKey)
+coin.addTransaction(tx1)
+
+console.log('\n Starting the miner...')
+coin.minePendingTransactions(myWalletAddress)
+console.log('\nBalance of cav is ', coin.getBalanceOfAddress(myWalletAddress))
 
 
-        console.log("Block successfully mined")
-        this.chain.push(block)
-        console.log(block)
-        this.pendingTransaction = []
-    }
+const tx2 = new Transaction(myWalletAddress, 'publickey goes here', 10)
+tx2.signTransaction(myKey)
+coin.addTransaction(tx2)
 
-    createTransaction(fromAddress, toAddress, data) {
-        let transaction = new Transaction(fromAddress, toAddress, data)
-        this.pendingTransaction.push(transaction)
-    }
+console.log('\n Starting the miner...')
+coin.minePendingTransactions(myWalletAddress)
+console.log('\nBalance of cav is ', coin.getBalanceOfAddress(myWalletAddress))
 
-}
 
-let prescription = new BlockChain();
-
-// prescription.addBlock(new Block(1, "somedate", { medication: "brufen" }));
-// prescription.addBlock(new Block(2, "somedate +1", { medication: "brufen" }));
-console.log("mining block 1....");
-prescription.createTransaction("doctor 0", "pacient 0 ", "medication: brufen")
-prescription.minePendingTransactions()
-console.log("mining block 2....");
-prescription.createTransaction("doctor 1", "pacient 1 ", "medication: viagra")
-prescription.minePendingTransactions()
-console.log("mining block 3....");
-prescription.createTransaction("doctor 2", "pacient 2 ", "medication: xanax")
-prescription.minePendingTransactions()
 
