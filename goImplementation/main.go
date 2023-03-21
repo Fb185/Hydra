534b01e0f0f35d5ae28bac1297f59b24c12b5a06
Fb185 <fredbobonejr@gmail.com>
2023-03-21 14:48:56 +0000

    block Logic

--- goImplementation/main.go
+++ goImplementation/main.go
@@ -1,6 +1,22 @@
 package main
 
-import(
-    "fmt"
-    "github.com/fb185/MediChain/goImplementation/blockchain"
+import (
+	"fmt"
+
+	"github.com/fb185/MediChain/goImplementation/blockchain"
 )
+
+
+func main() {
+	chain := blockchain.InitBLockChain()
+	chain.AddBlock("First")
+	chain.AddBlock("Second")
+	chain.AddBlock("Third")
+
+    for _, block := range chain.blocks{
+        fmt.Printf("Prev hash: %x\n", block.PrevHash)
+        fmt.Printf("Data in Block: %s\n", block.Data)
+        fmt.Printf("Hash : %x\n", block.Hash)
+    }
+
+}
